13abc88140774e1950263d3a8cf8fd028d93a43f
Tetiana Sobko <sobkotiana@gmail.com>
2024-08-25 01:02:36 +0300

    Fix flake8 warnings

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
+        self.x = round(x_coordinate, 2)
+        self.y = round(y_coordinate, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
@@ -53,9 +53,9 @@ class Vector:
         return round(math.degrees(math.acos(cos_a)))
 
     def rotate(self, degrees: int) -> Vector:
-        x = (math.cos(math.radians(degrees)) * self.x
-                   - math.sin(math.radians(degrees)) * self.y)
-        y = (math.sin(math.radians(degrees)) * self.x
-                   + math.cos(math.radians(degrees)) * self.y)
+        x_coordinate = (math.cos(math.radians(degrees)) * self.x
+                        - math.sin(math.radians(degrees)) * self.y)
+        y_coordinate = (math.sin(math.radians(degrees)) * self.x
+                        + math.cos(math.radians(degrees)) * self.y)
 
-        return Vector(x, y)
+        return Vector(x_coordinate, y_coordinate)
